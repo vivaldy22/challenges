6925f976a9b3e548f55ad6fd7f33cfe91be6d554
Vivaldy Andhira <dididlav@gmail.com>
2020-07-14 22:35:01 +0700

    berhitung dengan state done

--- src/components/card/MyCard.js
+++ src/components/card/MyCard.js
@@ -2,16 +2,54 @@ import React, { Component } from "react";
 import "./style.css";
 
 class MyCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+
+  tambah = () => {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  };
+
+  kurang = () => {
+    this.setState({
+      number: this.state.number - 1,
+    });
+  };
+
   render() {
-    const myCard = "myCard";
-    const button1Style = "button1Style";
-    const button2Style = "button2Style";
+    const num = this.state.number;
+    const isNumZero = num === 0;
+    const isNumLowerThanZero = num < 0;
 
     return (
-      <div className={myCard}>
+      <div className="myCard">
+        {isNumZero ? (
+          <p className="black">{num}</p>
+        ) : isNumLowerThanZero ? (
+          <p className="red">{num}</p>
+        ) : (
+          <p className="green">{num}</p>
+        )}
         <div>
-          <button className={button1Style}>-</button>
-          <button className={button2Style}>+</button>
+          <button
+            onClick={() => {
+              this.kurang();
+            }}
+          >
+            -
+          </button>
+          <button
+            onClick={() => {
+              this.tambah();
+            }}
+          >
+            +
+          </button>
         </div>
       </div>
     );
